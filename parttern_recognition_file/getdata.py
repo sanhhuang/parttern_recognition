2c0c58dc503a833daf0df004ed05aefbbc41d9d8
sanhhuang <sanhhuang@tencent.com>
2020-12-21 20:54:45 +0800

    fix

--- parttern_recognition_file/getdata.py
+++ parttern_recognition_file/getdata.py
@@ -1,8 +1,8 @@
-import readarrhythmia
+import readhcvdat0
+import readseeds_dataset
 import read_Sales_Transactions_Dataset_Weekly
-import readsemeion
 
 if __name__ == '__main__':
-    semeion = readsemeion.ReadSemeion('semeion.data')
-    arrhythmia = readarrhythmia.ReadArrhythmia('arrhythmia.data')
+    hcvdat0 = readhcvdat0.ReadHcvDat('hcvdat0.csv')
+    seeds_dataset = readseeds_dataset.ReadSeedsDataSet('seeds_dataset.txt')
     sales_transactions_dataset = read_Sales_Transactions_Dataset_Weekly.ReadSalesTransactionsDatasetWeekly('Sales_Transactions_Dataset_Weekly.csv')
