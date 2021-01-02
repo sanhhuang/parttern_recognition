8fc450628e83e7207ff7416350bb39567d70cc12
sanhhuang <68558164+sanhhuang@users.noreply.github.com>
2021-01-02 21:57:33 +0800

    Delete getdata.py

--- parttern_recognition_file/getdata.py
+++
@@ -1,8 +0,0 @@
-import readhcvdat0
-import readseeds_dataset
-import read_Sales_Transactions_Dataset_Weekly
-
-if __name__ == '__main__':
-    hcvdat0 = readhcvdat0.ReadHcvDat('hcvdat0.csv')
-    seeds_dataset = readseeds_dataset.ReadSeedsDataSet('seeds_dataset.txt')
-    sales_transactions_dataset = read_Sales_Transactions_Dataset_Weekly.ReadSalesTransactionsDatasetWeekly('Sales_Transactions_Dataset_Weekly.csv')
